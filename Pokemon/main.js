6de3aa32731defd26adf5707d86fc2c13da5957e
mvAlan <mv.alan4806@gmail.com>
2025-05-15 17:29:02 -0600

    Funcion asincrona para pokemon

--- Pokemon/main.js
+++ Pokemon/main.js
@@ -1,8 +1,8 @@
-BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'; 
+BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'; //Endpoint de la API
 
 //Crearemos la estructura del HTML 
 //Seleccionando los elementos del DOM
-
+//Botones para la paginación
 const container = document.getElementById("characters-container");
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
@@ -10,3 +10,20 @@ const nextButton = document.getElementById("next");
 //Variables para la paginación
 let currentPage = 1; //Pagina actual    
 let totalPages = 1; //Total de paginas
+
+//Funcion para obtener los pokemones
+async function getPokemons(page = 1) {
+    try {
+        //Solicitar los pokemones a la API
+        const response = await fetch(`${BASE_URL}?page=${page}`);
+        //en caso de error de respuesta
+        if (!response.ok) {
+            throw new Error(`HTTP ERROR: ${response.status} ${response.statusText}`);
+        }
+        //Extraer los datos de la respuesta de un JSON a un objeto de JavaScript
+    }
+    catch (error) {
+        //Mostrar un error en el contenedor de los personajes 
+        container.innerHTML = `<p> ❌ Error al obtener los pokemones: ${error.message} </p>`;
+    }
+}  
