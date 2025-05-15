331f3c9ef0c064f4c2f740aa716eadf916852cb4
mvAlan <mv.alan4806@gmail.com>
2025-05-15 01:34:26 -0600

    Se agrego uso del DOM

--- Pokemon/main.js
+++ Pokemon/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'; 
+
+//Crearemos la estructura del HTML 
+//Seleccionando los elementos del DOM
+
+const container = document.getElementById("characters-container");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+//Variables para la paginación
+let currentPage = 1; //Pagina actual    
+let totalPages = 1; //Total de paginas
